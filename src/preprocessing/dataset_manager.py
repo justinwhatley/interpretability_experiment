eaca703f66f91ed69668b3e6b37fc6d0cce12335
Justin Whatley <justinwhatley5@gmail.com>
2020-05-15 12:44:47 -0400

    Added optional overwrite functionality

---
+++ src/preprocessing/dataset_manager.py
@@ -0,0 +1,158 @@
+import dask.dataframe as dd
+import fastparquet
+from pathlib import PurePath
+from os.path import isdir
+from os import rmdir
+
+class DatasetManager():
+    """
+    Used to create a training and test set to be used by different scripts
+    """
+    
+    def __init__(self, config_obj):
+        self.config = config_obj
+        
+        self.set_directories(self.config)
+        self.set_targets()
+        
+        self.ddf = self.load_ddf()
+        self.ddf = self.make_modifications(self.ddf, self.config)
+            
+    
+    def set_directories(self, config):
+        self.input_path = config.input_path
+        
+        # Preprocessed directory
+        self.train_data_path = PurePath(config.train_data)
+        self.train_target_path = PurePath(config.train_target)
+
+        self.test_data_path = PurePath(config.test_data)
+        self.test_target_path = PurePath(config.test_target)    
+    
+    
+    def load_ddf(self):
+        return dd.read_parquet(self.input_path)
+        
+
+    def set_targets(self):
+        self.targets = self.config.targets
+        
+    
+    def make_modifications(self, ddf, config):
+        """
+        Modifies the columns before output to test and training sets
+        
+        mod_dict_keys = 'new_column_name', 'first_column', 'operation', 'second_column'
+        """
+        mods = config.to_modify
+        for mod in mods:
+            if mod['operation'].lower() == 'subtract':
+                ddf[mod['new_column_name']] = ddf[mod['first_column']] - ddf[mod['second_column']]
+            
+            elif mod['operation'].lower() == 'divide':
+                ddf[mod['new_column_name']] = ddf[mod['first_column']] / ddf[mod['second_column']]
+                
+            else:
+                print('Operation is not yet supported')
+        
+        return ddf
+        
+       
+    def _remove_dirs(self, paths_list):
+        """
+        Helper function to remove directory
+        """
+        print('Removing any old directories')
+        for path in paths_list:
+            try:
+                rmdir(path)
+            except OSError as e:
+                pass
+
+    def _check_all_paths_exist(self, paths_list):
+        """
+        Helper function to check whether all path already exist
+        This ensures dataset creation is not skipped when the files are not already there
+        """
+        for path in paths_list:
+            if not isdir(path):
+                return False
+        return True
+        
+
+    def write_dataset(self, test_size = 0.1, overwrite = False):
+        """
+        Write dataset to directory
+        """
+        
+        paths_list = [self.train_data_path, 
+                      self.train_target_path, 
+                      self.test_data_path, 
+                      self.test_target_path]
+        
+        all_paths_exist = self._check_all_paths_exist(paths_list)
+        
+        
+        if overwrite or not all_paths_exist: 
+            # Removes any full or partial datasets
+            self._remove_dirs(paths_list)
+            
+            # Writes training and test sets to disk
+            self.prepare_training_test(test_size)
+            self._write_dataset()
+        
+        else:
+            print('Not overwriting existing training and test sets')
+
+    def _write_dataset(self):
+        """
+        Helper function to write the actual dataset files
+        I've had better luck writing the files with pyarrow, reading with fastparquet
+        """
+        import pyarrow
+        engine = 'pyarrow'  
+        print('Writing trainings and test sets')
+        dd.to_parquet(self.X_train, self.train_data_path, engine=engine)
+        dd.to_parquet(self.y_train, self.train_target_path, engine=engine)
+
+        dd.to_parquet(self.X_test, self.test_data_path , engine=engine)
+        dd.to_parquet(self.y_test, self.test_target_path, engine=engine)
+    
+
+    
+    def prepare_training_test(self, test_size = 0.1):
+        """
+        Separates to training a test sets (once dask is run)
+        """
+        from dask_ml.model_selection import train_test_split
+    
+        y = self.ddf[self.targets]
+        X = self.ddf.drop(self.targets, axis=1)
+        
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, 
+                                                                                y, 
+                                                                                test_size = test_size,
+                                                                                random_state = 42)
+
+
+      
+    def get_training_set(self):
+        """
+        Get dask training set reader
+        """
+        self.X_train = dd.read_parquet(self.train_data_path)
+        self.y_train = dd.read_parquet(self.train_target_path)
+        
+        return self.X_train, self.y_train
+        
+        
+    def get_test_set(self):
+        """
+        Get dask test set reader
+        """
+        self.X_test = dd.to_parquet(self.X_test, self.test_data_path)
+        self.y_test = dd.to_parquet(self.y_test, self.test_target_path)
+        
+        return X_test, y_test
+    
+    
