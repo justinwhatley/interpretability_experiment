fb44a099e448df78653b208024151221d88e53b0
Justin Whatley <justinwhatley5@gmail.com>
2020-05-23 12:43:17 -0400

    Class to do datetime to categorical conversion. Currently only supports datetime to hourslot and datetime to day-of-week, but can easily expand to more, if needed.

---
+++ src/preprocessing/datetime_to_cat.py
@@ -0,0 +1,34 @@
+
+class DatetimeConverter():
+
+    def __init__(self):
+        self.categorical_columns_lst = []
+    
+    def add_datetime_categories(self, ddf, dt_column_lst, hourslot=True, day_of_week=True):
+        """
+        Extracts an integer representation for hourslot and/of day of week which is
+        turned into a categorical column
+        """
+        for column in dt_column_lst:
+            if hourslot:
+                ddf = self._add_hourslot_categorical(ddf, column)
+            if day_of_week:
+                ddf = self._add_day_of_week_categorical(ddf, column)
+                
+        return ddf, self.categorical_columns_lst
+
+
+    def _add_hourslot_categorical(self, ddf, dt_column):
+        new_column_name = dt_column + '_hourslot'
+        ddf[new_column_name] = ddf[dt_column].dt.hour
+        ddf[new_column_name] = ddf[new_column_name].astype('category')
+        self.categorical_columns_lst.append(new_column_name)
+        return ddf
+
+
+    def _add_day_of_week_categorical(self, ddf, dt_column):
+        new_column_name = dt_column + '_day_of_week'
+        ddf[new_column_name] = ddf[dt_column].dt.dayofweek
+        ddf[new_column_name] = ddf[new_column_name].astype('category')
+        self.categorical_columns_lst.append(new_column_name)
+        return ddf
