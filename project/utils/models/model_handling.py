af845c132b8f8fe4e3accde36a1944e289354d40
Justin Whatley <justinwhatley5@gmail.com>
2020-06-16 14:41:03 -0400

    Added a decorator function to load model if it exists (prior to training) or write the model to disk after training

---
+++ project/utils/models/model_handling.py
@@ -0,0 +1,32 @@
+import joblib
+
+def load_or_store_model(func, *args, **kwargs):
+    """
+    Wrapper to check whether the model is already saved to return saved model instead of new training
+    Function must have a save_to location and recompute option defined
+    """
+    
+    def loading_wrapper(*args, **kwargs):
+        recompute = kwargs['recompute']
+        save_to = kwargs['save_to']
+        
+        if not recompute:
+            try:
+                print('Loading previously trained model: ' + str(save_to))
+                return joblib.load(save_to)
+            except:
+                print('Model not found')
+        
+        model = func(*args, **kwargs)
+        return save_model(model, save_to)
+
+    def save_model(model, save_to):
+        print('Saving model to: ' + str(save_to))
+        joblib.dump(model, save_to)
+        return model
+
+    return loading_wrapper 
+
+
+
+
